6243555994eb9f5a0644676ce50f2bf4235ff391
Daniel Duton <daniel@duton.lu>
2016-06-03 15:52:11 +0200

    first basic socket server for testing

---
+++ src/manager.js
@@ -0,0 +1,17 @@
+const server = require('http').createServer();
+const io = require('socket.io')(server);
+const commandLineArgs = require('command-line-args');
+
+const optionDefinitions = [{
+    name: 'port',
+    alias: 'p',
+    type: Number
+}];
+const options = commandLineArgs(optionDefinitions);
+
+
+io.on('connection', function(socket) {
+    socket.on('connect', () => console.log('connect'));
+    socket.on('disconnect', () => console.log('disconnect'));
+});
+server.listen(options.port || 3000);
